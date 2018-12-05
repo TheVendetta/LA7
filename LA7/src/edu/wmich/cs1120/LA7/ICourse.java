54ded912882c0eaa7656c2e18993fa8d247a4c22
lucas <lucas@192.168.1.181>
2018-12-05 15:49:42 -0500

    Lucas: Updated coruse information

--- LA7/src/edu/wmich/cs1120/LA7/ICourse.java
+++ LA7/src/edu/wmich/cs1120/LA7/ICourse.java
@@ -1,5 +1,8 @@
 package edu.wmich.cs1120.LA7;
 
 public interface ICourse {
-
+	public boolean checkFull();
+	public void addStudent(String student);
+	public void printClassList();
+	public void checkClass();
 }
